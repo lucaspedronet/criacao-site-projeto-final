e632e72c1b7e18f4536c5fe53f166ad0b1c234cb
Lucas Pedro <lucaspedrolbg@gmail.com>
2024-10-23 20:38:43 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Getting Started with Create React App
 
+- Alterar Imagem 
+- Alterar Cor de Fundo 
+- Alterar Link de "Learn React" para "https://angular.dev"
+
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './pages/Main/main';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Parabéns vc conseguiu iniciar o projeto com o React.
         </p>
         <a
           className="App-link"
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Main />
     </div>
   );
 }
